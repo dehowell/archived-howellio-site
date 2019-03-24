b9daeb284501aefd51d19dd31e94694df4710d11
David Howell <dehowell@gmail.com>
2019-03-24 16:50:27 -0400

    Style 404 use emotion/styled

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,16 @@
-import { css } from "@emotion/core";
+import styled from "@emotion/styled";
 import React from "react";
 
 import Layout from "../components/layout";
 
+const Quote = styled.p`
+  font-style: italic;
+`;
+
 const NotFoundPage = () => (
   <Layout>
     <h1>Page Not Found</h1>
-    <p
-      css={css`
-        fontstyle: "italic";
-      `}
-    >
-      It is not down in any map; true places never are.
-    </p>
+    <Quote>It is not down in any map; true places never are.</Quote>
   </Layout>
 );
 
