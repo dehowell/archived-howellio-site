2e67b441d017386f2fa559fa177692649eae6c9a
David Howell <dehowell@gmail.com>
2019-03-26 20:08:17 -0400

    Add next/prev on blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -83,13 +83,18 @@ exports.createPages = ({ actions, graphql }) => {
 
     const posts = result.data.allMarkdownRemark.edges;
 
-    posts.forEach(post => {
+    posts.forEach((post, index) => {
+      const previous =
+        index === posts.length - 1 ? null : posts[index + 1].node;
+      const next = index === 0 ? null : posts[index - 1].node;
       createPage({
         path: post.node.fields.slug,
         component: templates[post.node.fields.source],
         context: {
           id: post.node.id,
-          slug: post.node.fields.slug
+          slug: post.node.fields.slug,
+          previous,
+          next
         }
       });
     });
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,54 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Link from "gatsby-link";
+import styled from "@emotion/styled";
+
 import Layout from "../components/layout";
 
-export default ({ data }) => {
+const PrevNextNav = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  margin: 0;
+`;
+
+const PrevLink = styled.li`
+  text-align: left;
+`;
+
+const NextLink = styled.li`
+  text-align: right;
+`;
+
+export default ({ data, pageContext }) => {
   const post = data.markdownRemark;
+  const { previous, next } = pageContext;
+
   return (
     <Layout>
       <article>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </article>
+      <PrevNextNav>
+        <PrevLink>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              {previous.frontmatter.title}
+              <br />
+              &larr;
+            </Link>
+          )}
+        </PrevLink>
+        <NextLink>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title}
+              <br /> &rarr;
+            </Link>
+          )}
+        </NextLink>
+      </PrevNextNav>
     </Layout>
   );
 };
