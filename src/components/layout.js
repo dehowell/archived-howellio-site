b54b33eeaef571be8233f1675f18da814b7742b5
David Howell <david.howell@coxautoinc.com>
2019-05-20 17:06:49 -0400

    Add a minimum margin around content

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,11 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default ({ children }) => (
-  <main style={{ margin: "3rem auto", maxWidth: 600 }}>
-    <Header />
-    {children}
-    <Footer />
-  </main>
+  <div style={{ padding: "1em" }}>
+    <main style={{ margin: "3rem auto", maxWidth: 600 }}>
+      <Header />
+      {children}
+      <Footer />
+    </main>
+  </div>
 );
