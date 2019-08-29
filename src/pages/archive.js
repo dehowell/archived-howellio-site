c427238ad9560b1865fbf057fec4a66eaccea568
David Howell <dehowell@gmail.com>
2019-08-29 15:51:22 -0400

    Organize archive by year

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,4 +1,5 @@
 import { graphql } from "gatsby";
+import _ from "lodash";
 import React from "react";
 
 import ArchiveLink from "../components/ArchiveLink";
@@ -7,33 +8,46 @@ import Layout from "../components/layout";
 
 export default function Archive({ data }) {
   const { edges: posts } = data.allMarkdownRemark;
+  const postsByYear = _.groupBy(posts, p => p.node.fields.year);
+
   return (
     <Layout>
       <Head title="Archive" />
+      <h2>Collections</h2>
       <ul>
         <ArchiveLink
           to={"/bibliography/d3"}
           title={"An annotated bibliography regarding D3.js"}
         />
       </ul>
-      <h2>Posts</h2>
-      <ul>
-        {posts
-          .filter(post => post.node.frontmatter.title.length > 0)
-          .map(({ node: post }) => {
-            const isFavorite = post.frontmatter.tags
-              ? post.frontmatter.tags.includes("favorite")
-              : false;
-            return (
-              <ArchiveLink
-                key={post.fields.slug}
-                to={post.fields.slug}
-                title={post.frontmatter.title}
-                favorite={isFavorite}
-              />
-            );
-          })}
-      </ul>
+      {Object.keys(postsByYear)
+        .sort()
+        .reverse()
+        .map(year => {
+          let posts = postsByYear[year];
+          return (
+            <section>
+              <h2>{year}</h2>
+              <ul>
+                {posts
+                  .filter(post => post.node.frontmatter.title.length > 0)
+                  .map(({ node: post }) => {
+                    const isFavorite = post.frontmatter.tags
+                      ? post.frontmatter.tags.includes("favorite")
+                      : false;
+                    return (
+                      <ArchiveLink
+                        key={post.fields.slug}
+                        to={post.fields.slug}
+                        title={post.frontmatter.title}
+                        favorite={isFavorite}
+                      />
+                    );
+                  })}
+              </ul>
+            </section>
+          );
+        })}
     </Layout>
   );
 }
@@ -49,7 +63,7 @@ export const pageQuery = graphql`
           id
           fields {
             slug
-            date(formatString: "MMMM DD, YYYY")
+            year: date(formatString: "YYYY")
           }
           frontmatter {
             title
